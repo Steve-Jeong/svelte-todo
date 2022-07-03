1dc6ab6f651b4dd0b9758ea6295d68b6f4d9176c
SteveJeong <jst0930@gmail.com>
2022-07-03 20:59:21 +0300

    add variable todo and bind to text input

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,16 @@
     {task:'Get eggs', isComplete:true, createdAt: new Date()},
     {task:'Get bread', isComplete:false, createdAt: new Date()},
   ]
+
+  let todo = {
+    task: '',
+    isComplete : false,
+    createdAt: new Date(),
+  }
 </script>
+
+<input type='text' placeholder="Add a task" bind:value={todo.task} />
+<button>Add</button>
 
 <ol>
   {#each todos as todo}
