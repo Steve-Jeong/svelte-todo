3e00a573b690f2c876777cb545686b15b7f89365
SteveJeong <jst0930@gmail.com>
2022-07-06 21:35:51 +0300

    initialize firebase, firestore

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDz087qb0APOVMYjztEVvPmQP2S-E0MZMU",
+  authDomain: "svelte-todo-b79b6.firebaseapp.com",
+  projectId: "svelte-todo-b79b6",
+  storageBucket: "svelte-todo-b79b6.appspot.com",
+  messagingSenderId: "810405731162",
+  appId: "1:810405731162:web:5aedea697369eac4dbf163"
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,44 +1,56 @@
 <script>
-  let todos = [ ]
+  import { initializeApp } from "firebase/app";
+  import { getFirestore } from "firebase/firestore";
+  import { firebaseConfig } from "$lib/firebaseConfig";
 
-  let task = ''
-  let error = ''
-  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
+  console.log({app, db});
+
+  let todos = [];
+
+  let task = "";
+  let error = "";
+
   const addTodo = () => {
-    if(task.trim() != '') {
+    if (task.trim() != "") {
       let todo = {
         task: task.trim(),
-        isComplete : false,
+        isComplete: false,
         createdAt: new Date(),
-      }
+      };
       todos = [...todos, todo];
-      error = ''
+      error = "";
     } else {
-      error = 'task is empty'
+      error = "task is empty";
     }
-    task = '';
-  }
+    task = "";
+  };
 
   const keyIsPressed = (e) => {
-    if(e.key === 'Enter') {
+    if (e.key === "Enter") {
       addTodo();
     }
-  }
-  
+  };
+
   const markTodoAsComplete = (index) => {
     // console.log(index)
     todos[index].isComplete = !todos[index].isComplete;
-  }
+  };
 
   const deleteTodo = (index) => {
     let deleteItem = todos[index];
-    todos = todos.filter((item) => item != deleteItem)
-  }
-  
-  $:console.table(todos)
+    todos = todos.filter((item) => item != deleteItem);
+  };
+
+  $: console.table(todos);
 </script>
 
-<input type='text' placeholder="Add a task" bind:value={task} />
+<input type="text" placeholder="Add a task" bind:value={task} />
 <button on:click={addTodo}>Add</button>
 
 <ol>
@@ -48,14 +60,14 @@
         {item.task} at {item.createdAt}
       </span>
       <span>
-        <button on:click={()=>markTodoAsComplete(index)}>✅</button>
-        <button on:click={()=>deleteTodo(index)}>❎</button>
+        <button on:click={() => markTodoAsComplete(index)}>✅</button>
+        <button on:click={() => deleteTodo(index)}>❎</button>
       </span>
     </li>
   {:else}
     <p>No todo found</p>
   {/each}
-  <p class='error'>{error}</p>
+  <p class="error">{error}</p>
 </ol>
 
 <svelte:window on:keydown={keyIsPressed} />
